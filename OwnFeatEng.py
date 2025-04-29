905f7ad0c635bca7d90480523c29f68399f92da5
Naxzele <iskandarsheraliyev@gmail.com>
2025-04-29 10:19:39 +0200

    added just clustering option

--- OwnFeatEng.py
+++ OwnFeatEng.py
@@ -3,7 +3,7 @@ from sklearn.cluster import KMeans
 import pandas as pd
 
 class feature_engineering:
-    def __init__(self, targets, transform_type, comp_type, comp_agg=None, groups=None, features=None, cl_name=None, clustering=False, cluster_range=None):
+    def __init__(self, targets=None, transform_type=None, comp_type=None, comp_agg=None, groups=None, features=None, cl_name=None, clustering=False, cluster_range=None):
         """
         Inputs:
             - targets: aggregating features
@@ -61,17 +61,18 @@ class feature_engineering:
                 self.kmeans[f'{self.cl_name}{cl}'] = KMeans(n_clusters=cl, n_init=50, random_state=0)
                 df[f'{self.cl_name}{cl}'] = self.kmeans[f'{self.cl_name}{cl}'].fit_predict(df[self.features])
 
-        if self.cl_name is not None:
-            self.transforms.update(self.GroupTransform(df, self.cl_name, self.targets, self.transform_type, self.cluster_range))
+        if self.targets is not None:
+            if self.cl_name is not None:
+                self.transforms.update(self.GroupTransform(df, self.cl_name, self.targets, self.transform_type, self.cluster_range))
 
-        if self.groups is not None:
-            for group in self.groups:
-                self.transforms.update(self.GroupTransform(df, group, self.targets, self.transform_type, cluster_range=None))
+            if self.groups is not None:
+                for group in self.groups:
+                    self.transforms.update(self.GroupTransform(df, group, self.targets, self.transform_type, cluster_range=None))
 
-        for key in self.transforms.keys():
-            df = df.merge(self.transforms[key], left_on=key.split('-')[1], right_index=True)
-            if self.comp_type is not None:
-                df = df.join(self.CompInGroup(df, key, self.comp_type, self.comp_agg))
+            for key in self.transforms.keys():
+                df = df.merge(self.transforms[key], left_on=key.split('-')[1], right_index=True)
+                if self.comp_type is not None:
+                    df = df.join(self.CompInGroup(df, key, self.comp_type, self.comp_agg))
 
         return df
 
@@ -80,9 +81,10 @@ class feature_engineering:
             for cl in self.cluster_range:
                 df[f'{self.cl_name}{cl}'] = self.kmeans[f'{self.cl_name}{cl}'].predict(df[self.features])
 
-        for key in self.transforms.keys():
-            df = df.merge(self.transforms[key], left_on=key.split('-')[1], right_index=True)
-            if self.comp_type is not None:
-                df = df.join(self.CompInGroup(df, key, self.comp_type, self.comp_agg))
+        if self.targets is not None:
+            for key in self.transforms.keys():
+                df = df.merge(self.transforms[key], left_on=key.split('-')[1], right_index=True)
+                if self.comp_type is not None:
+                    df = df.join(self.CompInGroup(df, key, self.comp_type, self.comp_agg))
 
         return df
